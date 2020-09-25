b38e36e7d9541315bb6875b07552c8669be90329
Ericssonenos <ericsson.eno@gmail.com>
2020-09-25 12:47:46 -0300

    Impementando calculadora Rest

---
+++ RestWithASPNETUdemy/Controllers/CalculatorController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestWithASPNETUdemy.Models;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+
+        [HttpGet("Sum/{firstNumber}/{secondNuber}")]
+        public string Sum(string firstNumber, string secondNuber) 
+        {
+            var V = new appModel(firstNumber, secondNuber);
+
+            if (V.Validation)
+            {
+                return "soma é :" + V.Result;
+
+            }
+
+            return "0";
+        }
+       
+    }
+}
---
+++ RestWithASPNETUdemy/Models/appModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace RestWithASPNETUdemy.Models
+{
+    public class appModel
+    {
+        public readonly double FistNumber;
+        public readonly double SecondNumber;
+        public readonly double Result;
+        public readonly bool Validation =false;
+
+        public appModel( string _firstnumber, string _secondNumber)
+        {
+
+            if (double.TryParse(_firstnumber, NumberStyles.Any, NumberFormatInfo.InvariantInfo, out FistNumber)
+            && double.TryParse(_secondNumber, NumberStyles.Any, NumberFormatInfo.InvariantInfo, out SecondNumber))
+                Validation = true;
+            Result = FistNumber + SecondNumber;
+        }
+
+      
+    }
+}
